40da817fdf6452c79f55deea1a62ca7686895030
Avdheshbhardwajz <140992329+Avdheshbhardwajz@users.noreply.github.com>
2024-04-28 17:35:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+let searchbtn = document.querySelector(".search");
+let itemsbox = document.querySelector(".items");
+let prevbtn = document.querySelector(".prev");
+let nextbtn = document.querySelector(".next");
+let departmentvalue = "";
+let gendervalue = "";
+let salaryvalue = "";
+let pagevalue = 10;
+let pagenumber = 1;
+
+prevbtn.addEventListener("click", function () {
+  pagenumber <= 1 ? undefined : pagenumber--;
+  fetchdata(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pagenumber}&limit=${pagevalue}${
+      departmentvalue
+        ? `&filterBy=department&filterValue=${departmentvalue}`
+        : ""
+    }${gendervalue ? `&filterBy=gender&filterValue=female` : ""}${
+      salaryvalue ? `&sort=salary&order=asc` : ""
+    }`
+  );
+});
+
+nextbtn.addEventListener("click", function () {
+  pagenumber > 10 ? undefined : pagenumber++;
+  fetchdata(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pagenumber}&limit=${pagevalue}${
+      departmentvalue
+        ? `&filterBy=department&filterValue=${departmentvalue}`
+        : ""
+    }${gendervalue ? `&filterBy=gender&filterValue=female` : ""}${
+      salaryvalue ? `&sort=salary&order=asc` : ""
+    }`
+  );
+});
+
+fetchdata(
+  `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pagenumber}&limit=${pagevalue}`
+);
+
+let department = document.querySelector("#department");
+
+department.addEventListener("change", function () {
+  departmentvalue = department.value;
+  fetchdata(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pagenumber}&limit=${pagevalue}&filterBy=department&filterValue=${departmentvalue}`
+  );
+});
+
+let gender = document.querySelector("#gender");
+
+gender.addEventListener("change", function () {
+  gendervalue = gender.value;
+  console.log(gendervalue);
+  fetchdata(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pagenumber}&limit=${pagevalue}&filterBy=gender&filterValue=${gendervalue}`
+  );
+});
+
+let salary = document.querySelector("#salary");
+
+salary.addEventListener("change", function () {
+  salaryvalue = salary.value;
+  fetchdata(
+    `https://dbioz2ek0e.execute-api.ap-south-1.amazonaws.com/mockapi/get-employees?page=${pagenumber}&limit=${pagevalue}&sort=salary&order=${salaryvalue}`
+  );
+});
+
+function showdata(daata) {
+  itemsbox.innerHTML = " ";
+  daata.data.forEach((ele) => {
+    let item = document.createElement("div");
+    item.classList.add("item");
+    item.innerHTML = `
+        <h3>${ele.id}</h3>
+        <h3>${ele.name}</h3>
+        <h3>${ele.gender}</h3>
+        <h3>${ele.department}</h3>
+        <h3>${ele.salary}</h3>`;
+    itemsbox.appendChild(item);
+  });
+}
+
+async function fetchdata(url) {
+  let response = await fetch(url);
+
+  let data = await response.json();
+  console.log(data);
+
+  showdata(data);
+}
